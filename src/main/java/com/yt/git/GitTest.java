2075e9b0746b1af5482d1ac8213bb4461c0e3e12
wdbkf <1228230079@qq.com>
2021-12-12 22:31:10 +0800

    first commit!

---
+++ src/main/java/com/yt/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yt.git;
+
+/**
+ * @author Yt
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git");
+    }
+}
